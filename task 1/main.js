ee8b3554f72f2884368dab085908accbe40c5892
sven6066 <sven609@mail.ru>
2023-12-16 04:22:29 +0300

    работает на половину

--- task 1/main.js
+++ task 1/main.js
@@ -1,19 +1,21 @@
-const images =[
+
+const images = [
     "https://i.pinimg.com/564x/8c/2f/c1/8c2fc1b5509c8f88239dd34ad8d56523.jpg",
     "https://i.pinimg.com/564x/16/36/d2/1636d287180a16b4abed0d2516255096.jpg",
     "https://i.pinimg.com/564x/15/e9/be/15e9bec59415e5fecc24b22445337734.jpg",
     "https://i.pinimg.com/564x/73/b8/ff/73b8ffb826befbfe5563b1b8a8d757b7.jpg"
-];
-const prevButton = document.querySelector('.prevButton');
-const nextButton = document.querySelector('.nextButton');
-
+  ];
 class Slider {
     constructor(images){
         this.images = images;
         this.currentSlider = 0;
+        this.slidesArr = []; // Добавляем массив для хранения DOM-элементов слайдов
         this.renderSlides();
-        prevButton.addEventListener('click', this.prevSlide());
+        this.prevButton = document.querySelector('.prevButton');
+        this.prevButton.addEventListener('click', this.prevSlide.bind(this)); // Привязываем контекст this
+        this.updateSlider();
     }
+
     renderSlides() {
         const slidesContainer = document.querySelector('.slides');
         this.images.forEach(image => {
@@ -21,21 +23,25 @@ class Slider {
           slide.classList.add('slide');
           slide.innerHTML = `<img src="${image}">`;
           slidesContainer.appendChild(slide);
+          this.slidesArr.push(slide); // Сохраняем созданный слайд в массиве slides
         });
-      }
-    
-    prevSlide(){
-        this.currentSlider = (this.currentSlider - 1 + this.images.length) % this.images.length;
+    }
+
+    prevSlide() {
+        this.currentSlider = (this.currentSlider - 1 + this.slidesArr.length) % this.slidesArr.length;
         this.updateSlider();
     }
+
     updateSlider() {
-        images.forEach((slide, index) => {
+        this.slidesArr.forEach((slide, index) => {
           if (index === this.currentSlider) {
             slide.style.display = 'block';
           } else {
             slide.style.display = 'none';
           }
         });
-      }
+    }
 }
+
+
 const slider = new Slider(images);
